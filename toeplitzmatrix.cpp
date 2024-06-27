a931d98acb98d0437041b6de7b8a7bd031aa7207
DEBANSHU007 <67229736+DEBANSHU007@users.noreply.github.com>
2024-06-27 19:07:38 +0530

    Add files via upload

---
+++ toeplitzmatrix.cpp
@@ -0,0 +1,14 @@
+bool isToeplitz(vector<vector<int>>& matrix) {
+    // code here
+     int m = matrix.size();
+        int n = matrix[0].size();
+        
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+}
