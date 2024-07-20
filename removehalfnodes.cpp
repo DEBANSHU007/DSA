20f3db74c70d2bcfec5cec87c2e8bf2301440613
DEBANSHU007 <67229736+DEBANSHU007@users.noreply.github.com>
2024-07-20 23:16:32 +0530

    Add files via upload

---
+++ removehalfnodes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    Node *RemoveHalfNodes(Node *root) {
+        // code here
+    if (root == nullptr) return nullptr;
+    
+    root->left = RemoveHalfNodes(root->left);
+    root->right = RemoveHalfNodes(root->right);
+    
+    if (root->left and !root->right) return root->left;
+    else if (root->right and !root->left) return root->right;
+    else return root;
+
+    }
+};
